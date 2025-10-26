aa8984d3ee179262b14e6a5f4fc378d92b71ed11
Gaelle Hisler <gaelle.hisler@umontpellier.fr>
2025-10-26 18:04:19 +0100

    ajout Blackjack.jav

---
+++ Blackjack.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Locale;
+import java.io.PrintStream;
+
+public class Blackjack {
+
+    public static Scanner input = new Scanner(System.in).useLocale(Locale.US);
+    public static PrintStream output = System.out;
+    public static Random random = new Random();
+
+    public static void main(String[] args) {
+        // votre aventure commence ici
+    }
+
+}
