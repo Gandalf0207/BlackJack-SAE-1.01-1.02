e3a2238d1fd716232e36c677280e44ac8247f940
Gandalf <quentin.pladeau@gmail.com>
2025-11-07 23:15:07 +0100

    ajout fichier de test

---
+++ Test/ScriptTest.py
@@ -0,0 +1,44 @@
+import subprocess
+
+def execution(listCmd):
+     
+    for cmd in listCmd:
+
+        try : 
+            resultat = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+            print(resultat.stdout.strip())
+        except Exception as e:
+            print("Execption : ", e)
+
+listCmd = [
+
+    # mail du 29 / 10 à 20h10
+    r'''find . -name Blackjack.java -exec bash -c "echo '0' | java {} | grep -q 'Réponse incorrecte \!' && echo {} OK || echo {} ÉCHEC" \; 2>/dev/null | sort''',
+
+    # mail du 30 / 10 à 21h28
+    r'''find . -name Blackjack.java -exec bash -c "jshell {} <<< $'double[][] résultat = Blackjack.playGame(3, 1); 32 16.5 100 0 0 0' | grep -q 'Error' && echo {} ÉCHEC || echo {} OK" \; 2>/dev/null | sort''',
+
+    # mail du 31 / 10 à 11h07
+    r'''find . -name Blackjack.java -exec bash -c "echo '2 3 0.99' | java {} | grep -q 'Réponse incorrecte' && echo {} OK || echo {} ÉCHEC" \; 2>/dev/null | sort''',
+
+    # mail du 01 / 11 à 22h44
+    r'''find . -name Blackjack.java -exec bash -c "javac {} -d . && javap Blackjack.class | grep -q 'boolean playRound(boolean\[\], double\[\], int\[\]);' && echo {} OK || echo {} ÉCHEC" \; 2>/dev/null | sort''',
+
+    # mail du 02 / 11 à 13h40
+    r'''find . -name Blackjack.java -exec bash -c "jshell {} <<< 'Blackjack.playersRemain(new boolean[]{false,true,false})' | grep -q '==> true$' && echo {} OK || echo {} ÉCHEC" \; 2>/dev/null | sort''',
+
+    # mail du 05 / 11 à 12h21
+    r'''find . -name Blackjack.java -exec bash -c "echo -e 'boolean[] playerIsActive = new boolean[]{true, true, true}; double[] playerMoney = {16, 32, 64}; double[] playerBet = new double[3]; Blackjack.collectBets(playerIsActive, playerMoney, playerBet);\n 4 8 16\n Arrays.equals(playerMoney, new double[]{12, 24, 48});' | jshell {} | grep -q '==> true' && echo {} OK || echo {} ÉCHEC" \; 2>/dev/null | sort''',
+    # mail du 05 / 11 à 21h00
+    r'''find . -name Blackjack.java -exec bash -c "echo '2 3 512.5 256.8' | java {} | grep -q 'PREMIÈRE PARTIE' && echo {} OK || echo {} ÉCHEC" \; 2>/dev/null | sort''',
+
+    # mail du 06 / 11 à 19h13
+    r'''find . -name Blackjack.java -exec bash -c "jshell {} <<< 'int[] T = new int[16]; T[0] = 9; Blackjack.cardsNumber(T) == 9' | grep -q '==> true' && echo {} OK || echo {} ÉCHEC" \; 2>/dev/null | sort''',
+
+    # mail du 07 / 11 à 22h02
+    r'''find . -name Blackjack.java -exec bash -c "jshell {} <<< 'int[] T = Blackjack.generateCards(1); var compteur = 0; for (var i = 0; i < T.length; i++) {if (T[i] == 7) compteur++;} T[0] == 52 && compteur == 4 && T.length == 53;' | grep -q '==> true' && echo {} OK || echo {} ÉCHEC" \; 2>/dev/null | sort'''
+]
+
+
+if __name__ == "__main__":
+    execution(listCmd)
