9cdce8031895509971d662bf2cd190492bc87f34
Kentus <quentin.pladeau@gmail.com>
2026-01-08 11:23:24 +0100

    Ajout Shoe.java

--- Livrable2/POOBJ/Shoe.java
+++ Livrable2/POOBJ/Shoe.java
@@ -1,39 +1,48 @@
-import java.util.ArrayList;
-import java.util.Collections;
+package POOBJ;
 
 /**
  * Représente le sabot (ensemble de cartes) utilisé par le croupier.
  * Remarque :
  * Dans le rendu 1, le mot deck désignait le sabot,
- *  ici, il désigne un paquet de 52 cartes... et c'est mieux en anglais ! ;-)
+ * ici, il désigne un paquet de 52 cartes... et c'est mieux en anglais ! ;-)
  */
 
-public class Shoe { 
+public class Shoe {
 
-    private CardSequence cards;   // Ensemble des cartes disponibles
-    private int numberOfDecks;    // Nombre de paquets de 52 cartes utilisés
-    
+    private CardSequence cards; // Ensemble des cartes disponibles
+    private int numberOfDecks; // Nombre de paquets de 52 cartes utilisés
+
     /**
      * Constructeur : Remplit le sabot avec n paquets de 52 cartes.
+     * 
      * @param n Nombre de paquets (entre 1 et 8).
      */
     public Shoe(int n) {
-	throw new RuntimeException("Méthode non implémentée ! Effacez cette ligne et écrivez le code nécessaire");
+        this.numberOfDecks = n;
+        this.cards = new CardSequence(n * 52, false);
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < 14; j++) {
+                for (int k = 0; k < 4; k++) {
+                    this.cards.addCard(new Card(j));
+                }
+            }
+        }
     }
 
     /**
      * Action : Remet toutes les cartes initiales dans le sabot et les mélange.
      */
     public void reset() {
-        throw new RuntimeException("Méthode non implémentée ! Effacez cette ligne et écrivez le code nécessaire");
+        this.cards.reset();
     }
-    
+
     /**
      * Action : Retire la carte du dessus du sabot.
+     * 
      * @return La carte tirée.
      */
     public Card drawCard() {
-        throw new RuntimeException("Méthode non implémentée ! Effacez cette ligne et écrivez le code nécessaire");
+        return this.cards.removeCard();
     }
 
 } // end class Shoe
